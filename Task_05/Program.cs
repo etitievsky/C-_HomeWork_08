0689d32d030926f14522c13bae3a21b0e4d98033
etitievsky <etitievsky@mail.ru>
2023-01-11 22:53:02 +0300

    Task 05

---
+++ Task_05/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(array[i, j]< 10){Console.Write($"0{array[i, j]} ");}
+            else{Console.Write($"{array[i, j]} ");}
+        }
+        Console.WriteLine("");
+    }
+}
+int [,] AddArrayRound(int [,] array)
+{
+    int n = array.GetLength(0);
+    int n_1 = array.GetLength(1);
+    int count = 1;
+    for (int i = 0, j = 0, k = 0; n != 0; i++,j++)
+    {
+        for (k = 0; k < n_1 - 1; k++) {array[i, j++] = count++;}
+        for (k = 0; k < n - 1; k++) {array[i++, j] = count++;}
+        for (k = 0; k < n_1 - 1; k++) {array[i, j--] = count++;}
+        for (k = 0; k < n - 1; k++) {array[i--, j] = count++;}
+        n = n < 2 ? 0 : n - 2;
+        n_1 = n_1 < 2 ? 0 : n_1 - 2;
+    }
+    return array;
+}
+
+int [,] array = new int [4,4];
+array = AddArrayRound(array);
+PrintArray(array);
+
